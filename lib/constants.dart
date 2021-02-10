406b6fa603189bfff9432c40a18e574b43ad1abc
Baxi19 <randald1991@gmail.com>
2021-02-10 11:04:16 -0600

    :octocat: Flutter Polyline App :fire:

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 
 class Constants{
-  static const String MAP_API_KEY = "AIzaSyATaX35-pRoADGirviYQZZSdPTGEFC4GvQ";
-  static const String API_KEY = "AIzaSyD06c5urg-aPTxl9SGM0X3BvlWl3afcUjk";
+  static const String MAP_API_KEY = "YOUR-API-KEY";
+  static const String API_KEY = "YOUR-API-KEY";
 }
